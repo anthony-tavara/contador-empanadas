a5f6cec6d285dd80698547d26f6580a77b554feb
Anthony Távara <atavara@fi.uba.ar>
2025-07-09 17:18:44 -0300

    Añadido: funcionalidad index.html optimizada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,7 @@
+const indexBody = document.getElementById('index-body')
+const icono_cambiar_modo_oscuro = document.getElementById('icono-luna-index')
 
-let indexBody=document.getElementById('index-body')
-let luna=document.getElementById('icono-luna-index')
-luna.addEventListener('click',cambiarModoIndex)
-
-indexBody.classList.add("modo-claro")
-let contadorCambiar=0
-
-function cambiarModoIndex(){
-    if(contadorCambiar==0){
-        indexBody.classList.add("modo-oscuro");
-        contadorCambiar+=1;
-    }else if(contadorCambiar==1){
-        indexBody.classList.remove("modo-oscuro");
-        indexBody.classList.add("modo-claro");
-        contadorCambiar+=-1;
-    }
-}
-
-function playAudioPlus(){
-    new Audio('../sounds/cambiar_modo.wav').play();
-}
-var botonIncio=document.getElementById('boton-inicio')
-luna.addEventListener('click',playAudioPlus)
-botonIncio.addEventListener('click',playAudioPlus)
+evento_agregar(icono_cambiar_modo_oscuro, "click", () => {
+    indexBody.classList.toggle("modo-oscuro")
+    sonido_reproducir(sonido_cambiar_modo_colores, 0, 1, false)
+})
