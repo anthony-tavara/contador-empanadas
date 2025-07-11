ff829b7f12106d7760b5b41bafcdb14fd489f2e9
Anthony Távara <atavara@fi.uba.ar>
2025-07-11 20:12:15 -0300

    Modificado: Funcionalidad anterior eliminada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,1 @@
-const indexBody = document.getElementById('index-body')
-const icono_cambiar_modo_oscuro = document.getElementById('icono-luna-index')
-
-evento_agregar(icono_cambiar_modo_oscuro, "click", () => {
-    indexBody.classList.toggle("modo-oscuro")
-    sonido_reproducir(sonido_cambiar_modo_colores, 0, 1, false)
-})
+const boton_iniciar_modo_simple = document.getElementById("boton-modo-simple")
